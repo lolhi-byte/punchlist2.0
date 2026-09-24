cffcd777253f1aeb05ed691f422a236ff7f1cdb8
lolhi-byte <lolhi@icloud.com>
2026-09-24 22:48:07 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -12,29 +12,40 @@ let currentList = null;
    function newList() {
     let listName = prompt("List name: ");
 
+    if (!listName) return;
+
     lists.push({
         name: listName,
         items: []
     });
+
     localStorage.setItem("lists", JSON.stringify(lists));
-        document.getElementById("listContainer").innerHTML +=
-            "<div>" +
-            "<button onclick='openList(\"" + listName + "\")'>" + listName + "</button>" +
-            "<button onclick='editList(" + (lists.length - 1) + ")'>Edit list</button>" +
-            "<button onclick='deleteList(" + (lists.length - 1) + ")'>Delete list</button>" +
-            "</div>";
+
+    document.getElementById("listContainer").innerHTML +=
+        "<div class='list-card'>" +
+            "<button class='list-name' onclick='openList(\"" + listName + "\")'>" + listName + "</button>" +
+            "<div class='list-actions'>" +
+                "<button class='edit-list' onclick='editList(" + (lists.length - 1) + ")'>Edit</button>" +
+                "<button class='delete-list' onclick='deleteList(" + (lists.length - 1) + ")'>Delete</button>" +
+            "</div>" +
+        "</div>";
 }
 function openList(listName){
     currentList = lists.find(function(list) {
         return list.name === listName;
     });
+
     document.getElementById("homeScreen").style.display = "none";
+
     document.getElementById("openListContainer").innerHTML =
-    "<button onclick='goBack()'>← Back</button>" +
-    "<h2>" + listName + "</h2>" +
-    "<button onclick='addItem()'>+ Add Item</button>" +
-    "<button onclick='printList()'>Print / PDF</button>" +
-    "<div id='itemContainer'></div>";
+        "<div id='listHeader'>" +
+            "<button onclick='goBack()'>← Back</button>" +
+            "<h2>" + listName + "</h2>" +
+            "<button onclick='addItem()'>+ Add Item</button>" +
+            "<button onclick='printList()'>Print / PDF</button>" +
+        "</div>" +
+        "<div id='itemContainer'></div>";
+
     displayItems();
 }
 function goBack() {
@@ -57,41 +68,65 @@ function addItem() {
     displayItems();
 }
 function displayItems() {
+    document.getElementById("listHeader").style.display = "block";
     document.getElementById("itemContainer").innerHTML = "";
 
     currentList.items.forEach(function(item, index) {
+
         document.getElementById("itemContainer").innerHTML +=
-            "<div class='report-item' id='item-" + index + "'>" +
-            "<p class='report-label'>ISSUE</p>" +
-            "<p class='report-issue'>" + item.issue + "</p>";
+            "<div class='job-row' onclick='openItem(" + index + ")'>" +
+                "<div class='job-thumbnail' id='thumbnail-" + index + "'></div>" +
+                "<div class='job-info'>" +
+                    "<div class='job-issue'>" + (item.issue || "") + "</div>" +
+                    (item.comment
+                        ? "<div class='job-comment'>" + item.comment + "</div>"
+                        : "") +
+                "</div>" +
+            "</div>";
 
-        if (item.comment) {
-            document.getElementById("item-" + index).innerHTML +=
-                "<p class='report-label'>COMMENT</p>" +
-                "<p class='report-comment'>" + item.comment + "</p>";
+        if (item.photos && item.photos.length > 0) {
+            displayThumbnail(item.photos[0], index);
         }
+    });
+}
+function openItem(index) {
+    let item = currentList.items[index];
+    document.getElementById("listHeader").style.display = "none";
+    document.getElementById("itemContainer").innerHTML =
+        "<button class='item-back' onclick='displayItems()'>← Back to list</button>" +
+        "<div class='expanded-item' id='item-" + index + "'>" +
+            "<h3>" + (item.issue || "") + "</h3>" +
+            "<div class='expanded-photos' id='photo-row-" + index + "'></div>" +
+        "</div>";
 
-        if (item.photos) {
-            document.getElementById("item-" + index).innerHTML +=
-                "<div class='photo-row' id='photo-row-" + index + "'></div>";
-
-            item.photos.forEach(function(photoId, photoIndex) {
-                document.getElementById("photo-row-" + index).innerHTML +=
-                    "<div class='photo-wrapper'>" +
+    if (item.photos) {
+        item.photos.forEach(function(photoId, photoIndex) {
+            document.getElementById("photo-row-" + index).innerHTML +=
+                "<div class='expanded-photo'>" +
                     "<div id='photo-" + photoId + "'></div>" +
                     "<button onclick='deletePhoto(" + index + ", " + photoIndex + ")'>Delete photo</button>" +
-                    "</div>";
+                "</div>";
 
-                displayPhoto(photoId);
-            });
-        }
+            displayPhoto(photoId);
+        });
+    }
 
+    if (!item.photos || item.photos.length < 3) {
+        document.getElementById("photo-row-" + index).innerHTML +=
+            "<button class='add-photo-tile' onclick='addPhoto(" + index + ")'>+</button>";
+    }
+
+    if (item.comment) {
         document.getElementById("item-" + index).innerHTML +=
-            "<button onclick='addPhoto(" + index + ")'>📷 Add Photo/s</button>" +
-            "<button onclick='editItem(" + index + ")'>Edit item</button>" +
-            "<button onclick='deleteItem(" + index + ")'>Delete item</button>" +
-            "</div>";
-    });
+            "<p class='expanded-comment-label'>COMMENT</p>" +
+            "<p class='expanded-comment'>" + item.comment + "</p>";
+    }
+
+    document.getElementById("item-" + index).innerHTML +=
+        "<div class='item-actions'>" +
+            "<button class='item-edit' onclick='editItem(" + index + ")'>Edit item</button>" +
+            "<button class='item-delete' onclick='deleteItem(" + index + ")'>Delete item</button>" +
+        "</div>";
 }
 function displayPhoto(photoId){
     let transaction = database.transaction(["photos"], "readonly");
@@ -104,11 +139,33 @@ function displayPhoto(photoId){
         "<img src='" + photoUrl + "' width='150'>";
     };
 }
+function displayThumbnail(photoId, index) {
+    let transaction = database.transaction(["photos"], "readonly");
+    let photoStore = transaction.objectStore("photos");
+    let getPhoto = photoStore.get(photoId);
+
+    getPhoto.onsuccess = function() {
+        let photo = getPhoto.result;
+
+        if (!photo) return;
+
+        let photoUrl = URL.createObjectURL(photo);
+
+        document.getElementById("thumbnail-" + index).innerHTML =
+            "<img src='" + photoUrl + "'>";
+    };
+}
 function deletePhoto(itemIndex, photoIndex) {
+    let confirmDelete = confirm("Delete this photo?");
+
+    if (!confirmDelete) {
+        return;
+    }
+
     currentList.items[itemIndex].photos.splice(photoIndex, 1);
 
     localStorage.setItem("lists", JSON.stringify(lists));
-    displayItems();
+    openItem(itemIndex);
 }
 function addPhoto(index) {
     if (!currentList.items[index].photos) {
@@ -131,7 +188,7 @@ function addPhoto(index) {
             let photoId = savePhoto.result;
             currentList.items[index].photos.push(photoId);
             localStorage.setItem("lists", JSON.stringify(lists));
-            displayItems();
+            openItem(index);
         };
     };
     photoInput.click();
@@ -184,7 +241,7 @@ function editItem(index) {
     currentList.items[index].comment = newComment;
 
     localStorage.setItem("lists", JSON.stringify(lists));
-    displayItems();
+    openItem(index);
 }
 function editList(index) {
     let currentName = lists[index].name;
@@ -209,12 +266,14 @@ function displayLists() {
     document.getElementById("listContainer").innerHTML = "";
 
     lists.forEach(function(list, index) {
-        document.getElementById("listContainer").innerHTML +=
-            "<div>" +
-            "<button onclick='openList(\"" + list.name + "\")'>" + list.name + "</button>" +
-            "<button onclick='editList(" + index + ")'>Edit list</button>" +
-            "<button onclick='deleteList(" + index + ")'>Delete list</button>" +
-            "</div>";
+    document.getElementById("listContainer").innerHTML +=
+        "<div class='list-card'>" +
+            "<button class='list-name' onclick='openList(\"" + list.name + "\")'>" + list.name + "</button>" +
+            "<div class='list-actions'>" +
+                "<button class='edit-list' onclick='editList(" + index + ")'>Edit</button>" +
+                "<button class='delete-list' onclick='deleteList(" + index + ")'>Delete</button>" +
+            "</div>" +
+        "</div>";
     });
 }
 let savedLists = localStorage.getItem("lists");
